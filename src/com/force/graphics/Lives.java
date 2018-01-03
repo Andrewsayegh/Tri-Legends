23cf29bcdad6a0810014852bcd6848da8f691f2c
zhu-alex <azuzhu2000@gmail.com>
2018-01-03 08:08:04 -0500

    Updated Lives

--- src/com/force/graphics/Lives.java
+++ src/com/force/graphics/Lives.java
@@ -12,17 +12,21 @@ import java.io.IOException;
  */
 public class Lives {
 
-    private BufferedImage heart;
+    private BufferedImage heart, emptyHeart;
 
     public Lives() {
         try {
             heart = ImageIO.read(new File("res/entity/heart.png"));
+            emptyHeart = ImageIO.read(new File("res/entity/emptyheart.png"));
         } catch (IOException e) {e.printStackTrace();}
     }
 
     public void drawHearts(Graphics2D g2, int lives){
-        for (int i = 0; i < lives; i++) {
-            g2.drawImage(heart, 30*i + 30, 50, null);
+        for (int i = 0; i < 3; i++) {
+            if(i < lives)
+                g2.drawImage(heart, 30*i + 30, 50, null);
+            else
+                g2.drawImage(emptyHeart, 30*i + 30, 50, null);
         }
     }
 }
