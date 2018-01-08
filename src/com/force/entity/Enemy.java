068b0ad6ca250aaefafd052cb4a59d093bad56b7
Wam <samuelwolff450@gmail.com>
2018-01-08 08:37:35 -0500

    Map

---
+++ src/com/force/entity/Enemy.java
@@ -0,0 +1,13 @@
+package com.force.entity;
+
+import com.force.graphics.Sprite;
+import com.force.util.Vector2f;
+
+/**
+ * Created by student on 12/20/17.
+ */
+//public class Enemy extends Entity {
+//    public Enemy(Sprite sprite, Vector2f orgin, int size) {
+//        super(sprite, orgin, size);
+//    }
+//}
